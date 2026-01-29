2bf406add902b64aa9f5e75585ff5a096917b08c
Faker389 <michal200614@gmail.com>
2026-01-29 15:56:30 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans antialiased `}>
+      <body>
         <ThemeProvider>
           <Suspense fallback={<p>Loading...</p>}>
           {children}
